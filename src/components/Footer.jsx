aee91f21c3834be860a2babe1129d2fccd0bc467
Kunal Garg <116939261+kunalgarg-18@users.noreply.github.com>
2023-07-22 19:54:24 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>GeekyTechy</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/kunal-garg-a07990225/" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://www.instagram.com/ig_kunal.garg/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/kunalgarg-18" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
